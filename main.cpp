d7e8bd7e474d23af7d388e048f3843e7396c3061
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2018-12-16 14:11:17 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main ()
+{
+    int p,l;
+    cout<<" Menghitung Luas Persegi Panjang " << endl;
+    cout<<" Masukan Nilai Panjang =";
+    cin>>p;
+    cout<<" Masukan Nilai Lebar =";
+    cin>>l;
+    cout<<"Luas Persegi Panjang =";
+    cout<<(p*l);
+    getch();
+    return 0;
+}
+
